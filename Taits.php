9fe56db1eac1e892af62d3ac359abd914f21f569
umg026 <umangwork26@gmail.com>
2024-08-10 11:28:58 +0530

    tarits in php"

--- Taits.php
+++ Taits.php
@@ -10,6 +10,27 @@ class abc{
 }
 $obj = new abc();
 $obj->sayHello();
+// =======================
 
+trait marks {
+    function result ($a, $b){
+     return ($a + $b)/2 ;
+    }
+}
+class countMarks{
+    use marks;
+    private int $m, $n;
+     function __construct($a,$b){
+        $this->m = $a;
+        $this->n = $b;
+    }
+    function percentage():float{
+        return $this->result($this->m, $this->n);
+    }
+  
+}
+$anil = new countMarks(50,60);
+// $anil->percentage();
+echo "Total marks is :".$anil->percentage();
 
 ?>
